b4b1121d14ccc3307606432d054d2163b875644d
mialimm <136047449+mialimm@users.noreply.github.com>
2023-06-22 12:00:07 +0800

    vs code file

--- ipa-1.py
+++ ipa-1.py
@@ -1,204 +1,98 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 2,
-   "id": "57eec2db-c386-476e-8b85-40c5b5a548f0",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "'''Individual Programming Assignment 1\n",
-    "\n",
-    "20 points\n",
-    "\n",
-    "This assignment will develop your basic familiarity with Python.\n",
-    "'''\n",
-    "\n",
-    "def savings(gross_pay, tax_rate, expenses):\n",
-    "    '''Savings.\n",
-    "    5 points.\n",
-    "\n",
-    "    This function calculates the money remaining\n",
-    "        for an employee after taxes and expenses.\n",
-    "\n",
-    "    To get the take-home pay of an employee, we will\n",
-    "        follow the following process:\n",
-    "        1. Apply the tax rate to the gross pay of the employee; round down\n",
-    "        2. Subtract the expenses from the after-tax pay of the employee\n",
-    "\n",
-    "    Parameters\n",
-    "    ----------\n",
-    "    gross_pay: int\n",
-    "        the gross pay of an employee for a certain time period, expressed in centavos\n",
-    "    tax_rate: float\n",
-    "        the tax rate for a certain time period, expressed as a number between 0 and 1 (e.g., 0.12)\n",
-    "    expenses: int\n",
-    "        the expenses of an employee for a certain time period, expressed in centavos\n",
-    "\n",
-    "    Returns\n",
-    "    -------\n",
-    "    int\n",
-    "        the number of centavos remaining from an employee's pay after taxes and expenses\n",
-    "    '''\n",
-    "    # Replace `pass` with your code.\n",
-    "    # Stay within the function. Only use the parameters as input. The function should return your answer.\n",
-    "    taxes = int(tax_rate*gross_pay)\n",
-    "    net = int(gross_pay-taxes-expenses)\n",
-    "    return net"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 16,
-   "id": "8fffb233-26e8-4e3d-b464-48fae1f99fe3",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "def material_waste(total_material, material_units, num_jobs, job_consumption):\n",
-    "    '''Material Waste.\n",
-    "    5 points.\n",
-    "\n",
-    "    This function calculates how much material input will be wasted\n",
-    "        after running a certain number of jobs that consume\n",
-    "        a set amount of material.\n",
-    "\n",
-    "    To get the waste of a set of jobs:\n",
-    "        1. Multiply the number of jobs by the material consumption per job.\n",
-    "        2. Subtract the total material consumed from the total material available.\n",
-    "\n",
-    "    The users of this function also want you to format the output as a string, annotated with the\n",
-    "        units in which the material is expressed. Do not add a space between the number and the unit.\n",
-    "\n",
-    "    Parameters\n",
-    "    ----------\n",
-    "    total_material: int\n",
-    "        the total material available\n",
-    "    material_units: str\n",
-    "        the units used to express a quantity of the material (e.g., \"kg\", \"L\", etc.)\n",
-    "    num_jobs: int\n",
-    "        the number of jobs to run\n",
-    "    job_consumption: int\n",
-    "        the amount of material consumed per job\n",
-    "\n",
-    "    Returns\n",
-    "    -------\n",
-    "    str\n",
-    "        the amount of remaining material expressed with its unit (e.g., \"10kg\").\n",
-    "    '''\n",
-    "    # Replace `pass` with your code.\n",
-    "    # Stay within the function. Only use the parameters as input. The function should return your answer.\n",
-    "    total_consumption = num_jobs*job_consumption\n",
-    "    waste = str(total_material-total_consumption)+str(material_units)\n",
-    "    return waste"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 23,
-   "id": "fbf94151-575f-4cf4-b473-4e2dbf305c2c",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "def interest(principal, rate, periods):\n",
-    "    '''Interest.\n",
-    "    5 points.\n",
-    "\n",
-    "    This function calculates the final value of an investment after\n",
-    "        gaining simple interest over a number of periods.\n",
-    "\n",
-    "    To calculate simple interest, simply multiply the principal to the quantity (rate * time).\n",
-    "        Add this amount to the principal to get the final value.\n",
-    "\n",
-    "    Round down the final amount.\n",
-    "\n",
-    "    Parameters\n",
-    "    ----------\n",
-    "    principal: int\n",
-    "        the principal (i.e., starting) amount invested, expressed in centavos\n",
-    "    rate: float\n",
-    "        the interest rate per period, expressed as a decimal representation of a percentage (e.g., 3% is 0.03)\n",
-    "    periods: int\n",
-    "        the number of periods invested\n",
-    "\n",
-    "    Returns\n",
-    "    -------\n",
-    "    int\n",
-    "        the final value of the investment\n",
-    "    '''\n",
-    "    # Replace `pass` with your code.\n",
-    "    # Stay within the function. Only use the parameters as input. The function should return your answer.\n",
-    "    simple_interest = int(principal*rate*periods)\n",
-    "    final = int(principal+simple_interest)\n",
-    "    return final"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 18,
-   "id": "60933403-557a-4b56-9dd3-0542c8724045",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "def body_mass_index(weight, height):\n",
-    "    '''Body Mass Index.\n",
-    "    5 points.\n",
-    "\n",
-    "    This function calculates the body mass index (BMI) of a person\n",
-    "        given their weight and height.\n",
-    "\n",
-    "    The formula for BMI is: kg / (m ^ 2)\n",
-    "        (i.e., kilograms over meters squared)\n",
-    "\n",
-    "    Unfortunately, the users of this function use the imperial system.\n",
-    "        You will need to first convert their arguments to the metric system.\n",
-    "\n",
-    "    Parameters\n",
-    "    ----------\n",
-    "    weight: float\n",
-    "        the weight of the person, in pounds\n",
-    "    height: list\n",
-    "        the height of the person, expressed as a list of two integers.\n",
-    "        the first integer is the foot component of their height.\n",
-    "        the second integer is the inches component of their height.\n",
-    "        for example, 5'10\" would be passed as [5, 10].\n",
-    "\n",
-    "    We have not yet discussed lists, but use the skills you developed\n",
-    "        in the command line exercise. How would you learn how to work with\n",
-    "        lists?\n",
-    "\n",
-    "    Returns\n",
-    "    -------\n",
-    "    float\n",
-    "        the BMI of the person.\n",
-    "    '''\n",
-    "    # Replace `pass` with your code.\n",
-    "    # Stay within the function. Only use the parameters as input. The function should return your answer.\n",
-    "    kg = weight/2.205\n",
-    "    m2 = ((int(str(height)[0])*12+int(str(height)[1]))*0.0254)**2\n",
-    "    bmi = kg/m2\n",
-    "    return bmi"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.10.9"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+def savings(gross_pay, tax_rate, expenses):
+    '''Savings.
+    5 points.
+
+    This function calculates the money remaining
+        for an employee after taxes and expenses.
+
+    To get the take-home pay of an employee, we will
+        follow the following process:
+        1. Apply the tax rate to the gross pay of the employee; round down
+        2. Subtract the expenses from the after-tax pay of the employee
+
+    Parameters
+    ----------
+    gross_pay: int
+        the gross pay of an employee for a certain time period, expressed in centavos
+    tax_rate: float
+        the tax rate for a certain time period, expressed as a number between 0 and 1 (e.g., 0.12)
+    expenses: int
+        the expenses of an employee for a certain time period, expressed in centavos
+
+    Returns
+    -------
+    int
+        the number of centavos remaining from an employee's pay after taxes and expenses
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    taxes = int(tax_rate*gross_pay)
+    net = int(gross_pay-taxes-expenses)
+    return net
+def material_waste(total_material, material_units, num_jobs, job_consumption):
+    '''Material Waste.
+    5 points.
+
+    This function calculates how much material input will be wasted
+        after running a certain number of jobs that consume
+        a set amount of material.
+
+    To get the waste of a set of jobs:
+        1. Multiply the number of jobs by the material consumption per job.
+        2. Subtract the total material consumed from the total material available.
+
+    The users of this function also want you to format the output as a string, annotated with the
+        units in which the material is expressed. Do not add a space between the number and the unit.
+
+    Parameters
+    ----------
+    total_material: int
+        the total material available
+    material_units: str
+        the units used to express a quantity of the material (e.g., "kg", "L", etc.)
+    num_jobs: int
+        the number of jobs to run
+    job_consumption: int
+        the amount of material consumed per job
+
+    Returns
+    -------
+    str
+        the amount of remaining material expressed with its unit (e.g., "10kg").
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    total_consumption = num_jobs*job_consumption
+    waste = str(total_material-total_consumption)+str(material_units)
+    return waste
+def interest(principal, rate, periods):
+    '''Interest.
+    5 points.
+
+    This function calculates the final value of an investment after
+        gaining simple interest over a number of periods.
+
+    To calculate simple interest, simply multiply the principal to the quantity (rate * time).
+        Add this amount to the principal to get the final value.
+
+    Round down the final amount.
+
+    Parameters
+    ----------
+    principal: int
+        the principal (i.e., starting) amount invested, expressed in centavos
+    rate: float
+        the interest rate per period, expressed as a decimal representation of a percentage (e.g., 3% is 0.03)
+    periods: int
+        the number of periods invested
+
+    Returns
+    -------
+    int
+        the final value of the investment
+    '''
+    # Replace `pass` with your code.
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    simple_interest = int(principal*rate*periods)
+    final = int(principal+simple_interest)
+    return final
